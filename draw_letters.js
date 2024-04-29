b2bed3b7045d64a5ba4fd095e5f43f2dc3e04df1
TaliaHope <161650309+TaliaHope@users.noreply.github.com>
2024-04-29 22:36:00 +1200

    Update draw_letters.js

--- draw_letters.js
+++ draw_letters.js
@@ -10,6 +10,7 @@ const color1 = "#A5D9FA";
 const color2 = "#A4422D";
 const color3 = "#32A42D";
 const color4 = "#00A77A";
+const color5 = "#F5D12C";
 const strokeColor = "#0a2d27";
 
 /*
@@ -48,6 +49,10 @@ function drawLetter(letterData) {
   let pos12x = 20 + letterData["offsetx10"];
   let pos13y = 50 + letterData["offsety11"];
   let pos13x = 40 + letterData["offsetx11"];
+ // let pos14y = 50 + letterData["offsety12"];
+ // let pos14x = 40 + letterData["offsetx12"];
+ // let pos15y = 20 + letterData["offsety13"];
+  //let pos15x = 80 + letterData["offsetx13"];
    
   fill(color1); // long line
   noStroke();
@@ -85,11 +90,11 @@ function drawLetter(letterData) {
   fill(color4); // 3 short line ro \
   rect(pos13x, pos13y, 90, 30);
 
-  //rotate(90);
-  //fill(color5); // 1 short line ro /
-  //rect(50, 0, 80, 35);
-  //fill(color5); // 2 short line ro /
-  //rect(50, -80, 80, 35);
+ // rotate(90);
+ // fill(color5); // 1 short line ro /
+ // rect(pos14x, pos14y, 80, 35);
+ // fill(color5); // 2 short line ro /
+  //rect(pos15x, pos15y, 80, 35);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
